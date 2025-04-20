6577e8914bafbb51962ca19a1e6223a361278ad9
Adnan Rashid <105010443+Adhijaan@users.noreply.github.com>
2025-04-19 20:24:44 -0400

    Time: 4 ms (58.18%), Space: 15.6 MB (66.74%) - LeetHub

--- 0974-reorder-data-in-log-files/0974-reorder-data-in-log-files.cpp
+++ 0974-reorder-data-in-log-files/0974-reorder-data-in-log-files.cpp
@@ -1,46 +1,47 @@
-class Solution {
-class log {
-public:
+struct LetterLog {
     string id;
-    string val;
-};
+    string log_letters;
+    LetterLog(string input){
+        int br = input.find_first_of(" ");
+        id = input.substr(0,br);
+        log_letters = input.substr(br);
+    }
 
-class comp {
-public:
-    bool operator()(const log& a, const log& b) const {
-        if(a.val != b.val){
-            return a.val < b.val;
+    bool operator>(const LetterLog & other){
+        if (log_letters > other.log_letters){
+            return true;
         }
-        return a.id < b.id;
+        if (log_letters == other.log_letters){
+            return id > other.id;
+        }
+        return false;
     }
 };
 
+class Solution {
 public:
     vector<string> reorderLogFiles(vector<string>& logs) {
-        vector<log> lets;
-        vector<log> digs;
-        
-        for (const string& l : logs) {
-            log entry;
-            int separator = l.find_first_of(' ');
-            entry.id = l.substr(0, separator);
-            entry.val = l.substr(separator + 1);
-            
-            if (isalpha(entry.val[0])) {
-                lets.emplace_back(entry); 
+        vector<string> l_logs;
+        vector<string> d_logs;
+        priority_queue<LetterLog, vector<LetterLog>, greater<>> pq;
+        for (const auto & log : logs){
+            int br = log.find_first_of(" ");
+            char content_first_char = log[br+1];
+            if (isalpha(content_first_char)){
+                pq.emplace(log);
             } else {
-                digs.emplace_back(entry); 
+                d_logs.push_back(log);
             }
         }
-        sort(lets.begin(), lets.end(), comp());
-
+        while (!pq.empty()){
+            LetterLog L = pq.top();
+            pq.pop();
+            string log = L.id + L.log_letters;
+            l_logs.push_back(log);
+        }
         vector<string> ans;
-        for (const log& l : lets) {
-            ans.emplace_back(l.id + " " + l.val); 
-        }
-        for (const log& l : digs) {
-            ans.emplace_back(l.id + " " + l.val); 
-        }
+        ans.insert(ans.end(), l_logs.begin(), l_logs.end());
+        ans.insert(ans.end(), d_logs.begin(), d_logs.end());
         return ans;
     }
 };
